128502b4450478d8532f16a22c561dc5e133d0f2
soumia <es_saadoun@esi.dz>
2017-10-20 03:17:53 +0100

    main modifié , la somme vérifiée avec l'exception , reste à revoir le tri

--- src/VectorHelper.java
+++ src/VectorHelper.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
 
+import vh.TDiffException;
+
 
 public class VectorHelper {
-
-	
-	
+	 public VectorHelper() {
+		
+	}
+	 
 		//Trier les �l�ments d�un vecteur
 		
 		public static void TriVec(int[] tab  ){
@@ -20,17 +23,18 @@ public class VectorHelper {
 		/*******************************************************************/
 		//Sommer deux vecteurs
 		  // une exception si les deux vecteurs ont des tailles diff�rentes
-		class TDiffException extends Exception{
-	     public void TaillesDiff(){
-		 System.out.println("les deux vecteurs ont des tailles diff�rentes!!");}}
-		//la somme
-		public int somme(int[] tab1 , int[] tab2) throws TDiffException{
-			if (tab1.length != tab2.length)throw new TDiffException();
-			else{
-			int somme = 0; int i;
+		/*class TDiffException extends Exception{
+	     public void TaillesDiff(java.lang.String[] args){
+		 System.out.println("les deux vecteurs ont des tailles diff�rentes!!");}}//la somme
+	  */   
+		public static  void somme(int[] tab1 , int[] tab2) throws TDiffException{
+			if (tab1.length != tab2.length) {
+				throw new TDiffException();
+			} else {
+				int somme = 0; int i;
 				for (i=0;i<tab1.length;i++){somme = somme+tab1[i];}
 				for (i=0;i<tab2.length;i++){somme = somme+tab2[i];}
-			return somme;}}
+				System.out.println("la somme des elements >> " +somme ); }}
 		/*******************************************************************/
 		//Inverser les �l�ments d�un vecteur
 		public int[] inverse(int[] tab){
@@ -48,7 +52,7 @@ public class VectorHelper {
 		
 
 		
-		public static void main(String[] args) {
+		public static void main(String[] args) throws Exception {
 
 			System.out.println("Choisissez une operation :  \n");
 			System.out.println(" [1] . Trier les éléments d’un vecteur \n");
@@ -58,6 +62,7 @@ public class VectorHelper {
 			System.out.println(" [5] . Appliquer une formule à tous les éléments d’un vecteur\n");
 			Scanner sc = new Scanner(System.in); int sc1 = sc.nextInt();
 			switch (sc1){
+			
 			    case (1):
 			    	System.out.println("le nombre d'elements dans le vecteur");
 			        Scanner nb = new Scanner(System.in); int nbr = nb.nextInt();
@@ -66,11 +71,28 @@ public class VectorHelper {
 	                	Scanner j = new Scanner(System.in); int k = j.nextInt();
 	                	tab[i]=k; }
 	                 TriVec(tab);
-	               
-			       
-
-	                break;
-	            case (2):  break;
+			          break;
+	            case (2):
+	            	System.out.println("le nombre d'elements du 1er vecteur");
+	            Scanner nb01= new Scanner(System.in); 
+	            int nb1 = nb01.nextInt();
+	            System.out.println("le nombre d'elements du 2eme vecteur");
+	            Scanner nb02= new Scanner(System.in); 
+	            int nb2 = nb02.nextInt();
+	            if (nb1==nb2){
+	            	int[] tab1 = new int[nb1];int[] tab2 = new int[nb2];
+	            	System.out.println(" Remplir le 1er vect");
+	            	for (int i=0;i<nb1;i++){
+	      	            		Scanner e= new Scanner(System.in);
+	            		int ee = e.nextInt(); tab1[i]=ee;}
+	            	System.out.println(" Remplir le 2eme vect");
+	            	for (int i=0;i<nb1;i++){
+	            		
+	            		Scanner e= new Scanner(System.in);
+	            		int ee = e.nextInt(); tab2[i]=ee;}
+	            	   somme(tab1 ,tab2);
+	            }else throw new TDiffException();
+	            	break;
 	            case (3):  break;
 	            case (4):  break;
 	            case (5):  break;
---
+++ src/vh/TDiffException.java
@@ -0,0 +1,7 @@
+package vh;
+
+public class TDiffException extends Exception {
+	public TDiffException(){
+		 System.out.println("les deux vecteurs ont des tailles differentes!!");}
+
+}
